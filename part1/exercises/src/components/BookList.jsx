58d85e51f4f8198238e3a05d3a69cb32af1454b2
StaceyO21 <staceyobrien21@gmail.com>
2024-08-20 23:20:50 -0500

    react

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Sucka";
+   let book1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcesBopsy0G1DlgqgY-fwyaAWotjjm454sXw&s";
+   let book2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtVtu6EtpHF5Mi7J3oPH7MR9eg6onKkX7pCQ&s";
+   let book3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFHGG25szuc-ffdnm3ZIt8AxbzDuxzxNb3Vw&s";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="This is Why We Lied by Karin Slaughter" />
+         <img src={book2} alt="You Like it Darker by Stephen King" />
+         <img src={book3} alt="The Book of Bill by Alex Hirsch" />
       </div>      
    );
 }
